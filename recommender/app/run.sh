13ea1c5197ce4928eda98117757d1bbef9464502
glongrais <guillaume.longrais@orange.fr>
2020-06-28 16:21:52 +0200

    fix and readme creation

--- recommender/app/run.sh
+++ recommender/app/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-OPT="../../tmp/coclust6"
 CONFIG="test_config.yml"
 
-mkdir -p $OPT
-
-java -jar "target/binder-0.0.1-SNAPSHOT.jar" #--config="$CONFIG" -DOPT="$OPT"
+java -jar "target/binder-0.0.1-SNAPSHOT.jar" #--config="$CONFIG" 
