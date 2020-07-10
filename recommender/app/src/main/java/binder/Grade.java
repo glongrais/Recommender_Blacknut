17ed0bb9c97d5ebe1a224e0f970563da451c9de4
Guillaume L <34845137+glongrais@users.noreply.github.com>
2020-07-10 10:42:20 +0200

    Update Grade.java

--- recommender/app/src/main/java/binder/Grade.java
+++ recommender/app/src/main/java/binder/Grade.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2020 Guillaume Longrais & Robinson Sablon De Gelis
+    Copyright 2020 Guillaume Longrais & Robinson Sablons De Gelis
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
